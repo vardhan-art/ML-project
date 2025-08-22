208a998f45ee0e46f2510a2ecdf28b96e6dcd2b0
vardhan-art <kalurivardhan@gmail.com>
2025-08-22 15:19:44 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup,find_packages
+with open("Requirements.txt") as f:
+    Requirements=f.read().splitlines()
+
+    setup(
+        name="Smart Home Energy Management",
+        version="0.1",
+        author="K.Vardhan",
+        packages=find_packages(),
+        install_requires=Requirements,
+    )
